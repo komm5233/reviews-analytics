7c01f8ae8505c01c9f1543228ecec593e1fe01c3
Matt Chien <komm5233@hotmail.com>
2022-04-05 21:08:42 +0800

    add word count function

--- read.py
+++ read.py
@@ -32,3 +32,38 @@ for d in data:
 		good.append(d)
 print('一共有',len(good), '筆留言提到GOOD')
 print(good[0])
+
+
+# 文字計數
+wc = {} # word_count
+for d in data:	
+	words = d.split(' ')
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:
+			wc[word] = 1 # 新增新的KEY進WC字典 
+
+
+for word in wc:
+	if wc[word] > 1000000:
+		print(word, wc[word])
+
+
+print(wc['Matt'])
+
+
+while True:
+	word = input('請輸入你想查什麼字:')
+	if word == 'q':
+		break
+	if word in wc:
+		print(word , '出現的次數為:', wc[word])
+	else:
+		print('沒出現過這字喔!')
+
+print('感謝使用本次查詢功能')
+
+
+
+
